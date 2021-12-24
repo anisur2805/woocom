272a204821292f56db9b9b94d9274cee64396c3a
anisur2805 <anisur2805@gmail.com>
2021-12-25 03:58:15 +0600

    move wc style file

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -43,7 +43,7 @@ add_action( 'after_setup_theme', 'woocom_woocommerce_setup' );
  * @return void
  */
 function woocom_woocommerce_scripts() {
-	wp_enqueue_style( 'woocom-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION );
+	wp_enqueue_style( 'woocom-woocommerce-style', get_template_directory_uri() . '/css/woocommerce.css', array(), _S_VERSION );
 
 	$font_path   = WC()->plugin_url() . '/assets/fonts/';
 	$inline_font = '@font-face {
