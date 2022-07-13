3f8acce9948a70aa11702da1498cce21317a6828
anisur2805 <anisur2805@gmail.com>
2022-07-13 21:48:16 +0600

    a.c

--- template-parts/content.php
+++ template-parts/content.php
@@ -7,7 +7,7 @@
  * @package woocom
  */
 
-?>
+?>                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
