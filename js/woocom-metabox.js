c7919e7cc752ecb74df567d4ffc9bcf00ccfb6bd
anisur2805 <anisur2805@gmail.com>
2022-07-20 18:36:45 +0600

    add gitignore

--- js/woocom-metabox.js
+++ js/woocom-metabox.js
@@ -1,22 +1,20 @@
+/*jslint browser: true, plusplus: true */
 (function ($, window, document) {
     'use strict';
-    // execute when the DOM is ready
     $(document).ready(function () {
-		$("#woocom_filed").on('change', function () {
-			$.post(woocom_meta_box_obj.url, {
-				action: woocom_meta_box_action,
-				woocom_meta_box_value: $('#wporg_field').val(),
-				post_ID: jQuery('#post_ID').val()   
-
-			}, function( data) {
-				if(data=='success'){
-					console.log( 'data ', data )
-				} else if(data=='failure'){
-					console.log( 'sad' );
-				} else {
-					console.log( 'nothing' );
-				}
-			})
-		})
-		})
-	}(jQuery, window, document));
+        $('#wporg_field').on('change', function () {
+            $.post(woocom_meta_box_obj.url,
+                   {
+                       action: 'wporg_ajax_change', 
+                       wporg_field_value: $('#wporg_field').val(), 
+                       post_ID: $('#post_ID').val()
+                   }, function (data) {
+                        if (data === 'success') {
+                        } else if (data === 'failure') {
+                        } else {
+                        }
+                    }
+            );
+        });
+    });
+}(jQuery, window, document));
