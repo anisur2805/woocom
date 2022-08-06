f84cbf83c380034a763802794a4827aad5d6d671
anisur2805 <anisur2805@gmail.com>
2022-08-07 02:12:19 +0600

    newsletter updated

--- js/main-script.js
+++ js/main-script.js
@@ -21,7 +21,7 @@ var swiper = new Swiper(".mySwiper", {
 // Product Cats carousel
 // eslint-disable-next-line no-unused-vars
 const swiperShopByCat = new Swiper(".wc__shop_by_cat_inner", {
-    slidesPerView: 6,
+    slidesPerView: 2,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -30,6 +30,24 @@ const swiperShopByCat = new Swiper(".wc__shop_by_cat_inner", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
   });
 
 var swiper = new Swiper(".mySwiper", {
