4fab3b97a5c6fc21d4d65362844a65837959b3ae
anisur2805 <anisur2805@gmail.com>
2022-11-28 12:41:58 +0600

    partner logo

--- template-parts/content-front-page.php
+++ template-parts/content-front-page.php
@@ -18,23 +18,23 @@ if( !function_exists('is_plugin_active') ) {
 
 	<?php echo get_template_part('/template-parts/shop-by', 'category'); ?>
 	
-	<?php // echo get_template_part('/template-parts/about' ); ?>
+	<?php echo get_template_part('/template-parts/about' ); ?>
 	
 	<?php get_template_part('/template-parts/support', 'area'); ?>
 
-	<?php // echo get_template_part('/template-parts/ajax-load-more' ); ?>
+	<?php //echo get_template_part('/template-parts/ajax-load-more' ); ?>
 	
-	<?php // echo get_template_part('/template-parts/custom-posts' ); ?>
+	<?php //echo get_template_part('/template-parts/custom-posts' ); ?>
 
 	<?php get_template_part('/template-parts/product', 'area'); ?>
 	
-	<?php get_template_part('/template-parts/promotion'); ?>
+	<?php //get_template_part('/template-parts/promotion'); ?>
 	
-	<?php get_template_part('/template-parts/featured', 'cat'); ?>
+	<?php //get_template_part('/template-parts/featured', 'cat'); ?>
 	
-	<?php get_template_part('/template-parts/inspirational', 'post'); ?>
+	<?php //get_template_part('/template-parts/inspirational', 'post'); ?>
 	
-	<?php // get_template_part('/template-parts/subscribe'); ?>
+	<?php get_template_part('/template-parts/subscribe'); ?>
 
 	<?php
 	if ( is_plugin_active( 'wpforms-lite/wpforms.php' ) ) :
--- template-parts/partners-logo.php
+++ template-parts/partners-logo.php
@@ -11,23 +11,33 @@
 
 	<section class="partners-logo">
 		<div class="container text-center">
-			<h2>Partners Logo</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, suscipit ullam dolorem non debitis ipsum!</p>
+			<h2><?php _e( 'Partners Logo', 'woocom' ); ?></h2>
+			<p><?php _e('Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, suscipit ullam dolorem non debitis ipsum!', 'woocom'); ?></p>
 		</div>
 		<div class="container marginTop40px">
 			<div class="row">
-				<?php 
+				<?php
+					$index = 0;
 					foreach( $logo_names as $logo_label => $logo_name ) {
-						$setting_id = sprintf('woocom_%s', $logo_name );
-						$logo_url = get_theme_mod( $setting_id );
+						$setting_id 	= sprintf('woocom_%s', $logo_name );
+						$logo_url 		= get_theme_mod( $setting_id );
+						$default_url    = get_template_directory_uri() . '/assets/images/hero-slider/1.webp';
 						if( $logo_url ):
 						?>
-							<div class="col-md-3 col-xs-6 mb-4">
+							<div class="col-md-3 col-xs-6 mb-4 a">
 								<div class="partner-logo">
 									<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_attr( $logo_label ); ?>" class="img-responsive" />
 								</div>
 							</div>
 						<?php
+						else:
+							?>
+							<div class="col-md-3 col-xs-6 mb-4 b">
+								<div class="partner-logo">
+									<img src="<?php echo esc_url( $default_url ); ?>" alt="Logo" class="img-responsive" />
+								</div>
+							</div>
+							<?php
 						endif;
 					}
 				?>
